6193dd4deca2c5397c3564b4b42b729e1d103671
Pritam Maity <pritammaity123456@gmail.com>
2018-04-26 12:52:36 +0530

    Add files via upload

---
+++ httdocs/index.php
@@ -0,0 +1,20 @@
+<?php require 'lib/connection.php'; ?>
+<html>
+<head>
+<link rel="icon" href="favicon.ico">
+ <meta charset="utf-8">
+ <link rel="stylesheet" type="text/css" href="lib/style.css">
+ <title>Synergic Softek Solutions</title>
+</head>
+<body>
+	
+	
+    <div class="container">
+    <div class="header"><a href="index.php"><img src="syn_header.png"/></a></div>
+    <div class="content"><a class="index_btn" href="<?php echo $l_softek_index; ?>">SOFTEK</a></div>
+	<div class="content"><a class="index_btn1" href="<?php echo $l_dds_index; ?>">DDS</a></div>
+    </div>
+  
+</body>
+</html>
+
